c416030104b15cb87a5d08b1701400366b853c2d
EthanNC <Ethan_Cumming@student.uml.edu>
2019-12-30 17:28:37 -0500

    site content

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,8 +74,22 @@ const IndexPage = () => (
       margin:"2rem auto",
       textShadow: " 2px 2px 4px #000000"
     }}>
+      <img style={{margin:0, borderRadius: '10px' }} src={image2}/>
+<p>There are two salient features of the TME that are worth noting as regards oncogenesis, tumor growth, and metastasis. First, the TME is crucial for disease progression which relies upon a carefully orchestrated molecular conversation between cancerous and non-cancerous tumor elements to achieve its goals. For example, both cancer cells and their non-cancerous tumor constituents of the TME act together to suppress the normal immune response that would otherwise hold a cancer in check or destroy it. Second, just as cancer is made up of hundreds of different, yet related, diseases, the TME is distinct in its cellular and constituent components in different cancers. Important current concepts in cancer, such as the existence of “hot” or “cold” tumors that respond differently to immunotherapies, have distinct TMEs. For both of these reasons, the TME is an active target in the search for new cancer therapies. </p>
+      </div>
+      <div     style={{
+      padding: ".5rem 2.5rem",
+      color: COLORS.lightWhite,
+      fontWeight: 400,
+      background: GRADIENT,
+      borderRadius: BORDER_RADIUS,
+      borderWidth: 0,
+      cursor: "pointer",
+      margin:"2rem auto",
+      textShadow: " 2px 2px 4px #000000"
+    }}>
       <img style={{margin:0, borderRadius: '10px' }} src={image4}/>
-<p>There are two salient features of the TME that are worth noting asregards oncogenesis,tumor growth,and metastasis. First, the TME is crucial for disease progression which relies upon a carefully orchestrated molecular conversation between cancerous and non-cancerous tumor elements to achieve its goals. For example, both cancer cells and their non-cancerous tumor constituentsof the TMEact together to suppress the normal immune response that would otherwise hold a cancer in check or destroy it. Second, just as cancer is made up of hundredsof different, yet related, diseases, the TME is distinctin its cellular and constituent componentsin different cancers. Important current concepts in cancer, such as the existence of “hot” or “cold” tumors that respond differently to immunotherapies, have distinct TMEs. For both of these reasons, the TME is an active target in the search for new cancer therapies</p>
+<p>Sugars - covalently linked to proteins, lipids, and each other - are termed glycans and are important molecular participants in cancer, including as components of the TME. Here, glycans participate in a distinct set of molecular conversations by encoding information required for disease progression within their structural details – information that is decoded by a set of proteins called lectins that are differentially expressed on cancer cells and in the TME.  An emerging body of scientific research supports the concept that by altering the glycans in a tumor one can disrupt the ongoing cancer-enabling molecular conversations. </p>
       </div>
       <h2 style={{textAlign:"center"}}>Our Mission</h2>
       <div     style={{
@@ -89,8 +103,9 @@ const IndexPage = () => (
       margin:"2rem auto",
       textShadow: " 2px 2px 4px #000000"
     }}>
+      <p>Nashoba Biotechnology, LLC is founded on the concept that altering specific glycan structures will disrupt essential molecular conversations and create a new class of anti-cancer drugs. The specific approach embraced by the Company is termed precision glyco-editing of the TME. This approach involves inducing specific structural changes in a small subset of TME glycans and has rarely been exploited before.  However, this approach is now supported by studies showing that Nashoba’s specific molecular targets are likely inverse prognostic indicators of overall survival in various cancers. </p>
       <img style={{margin:0, borderRadius: '10px'}} src={image3}/>
-      <p>Nashoba Biotechnology, LLC is re-developing a clinical stage cancer therapeutic that has shown efficacy in early stage phase II clinical trials in metastatic renal cancer. There is now emerging evidence that when used in combination with approved immunotherapies, such as anti-VEGF antibodies or ICIs, Nashoba Biotech's NB-0039 will increase the durable response rates of these agents by precision editing of the tumor microenvironment. Thus, NB-0039 is a “helper drug” that assists approved therapeutics in achieving better clinical outcomes in cancer.</p>
+      <p>Nashoba Biotechnology, LLC is re-developing a therapeutic candidate that has previously shown safety and efficacy in early stage phase II clinical trials in metastatic cancer. The Company believes that, if used in combination with approved immunotherapies such as anti-VEGF antibodies or ICIs, its lead clinical candidate will increase the durable response rates of these agents by precision glyco-editing of the TME. Thus, we envisage our clinical candidate as a “helper drug” that assists approved therapeutics in achieving even better clinical outcomes for cancer patients. </p>
       
       </div>
     </div>
